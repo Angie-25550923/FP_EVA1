0a3f30c5af48d47f0d6439b27eb8a7233ca8cb2f
Angie <vett200701@gmail.com>
2025-09-03 09:49:18 -0600

    Add files via upload

---
+++ EVA1_2_DATOS/src/eva1_2_datos/EVA1_2_DATOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_datos;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA1_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //;significa fin de linea
+        //"  " =cadena de texto. dato
+        System.out.println("ANGELES ANTONIO");
+        //Numeros
+        System.out.println(1000);
+        //Numeros decimales
+        System.out.println(500.3);
+        //valores logicos(verdadero o falso)
+        //true or false
+        System.out.println(true);
+        //Caracteres individuales
+        //cosas distintas
+        System.out.println('c'); //Caracter
+        System.out.println("c"); ///Cadena de texto 
+    }
+    
+}
