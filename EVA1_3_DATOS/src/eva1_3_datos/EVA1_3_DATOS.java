86edbe4e15335a04171cdd0cb65d4bca87e75f3e
Angie <vett200701@gmail.com>
2025-09-03 09:50:08 -0600

    Add files via upload

---
+++ EVA1_3_DATOS/src/eva1_3_datos/EVA1_3_DATOS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_datos;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA1_3_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("NOMBRE DEL ALUMNO:");
+        System.out.println("ANGELES ANTONIO");
+        System.out.print("NUMERO DE CONTROL:");
+        System.out.println(25550923);
+        System.out.print("EDAD:");
+        System.out.println(17);
+        //print=en el mismo renglon
+        //println=cambia de renglon
+        
+    }
+    
+}
