f1c82a1f6bea0146f8efa2e4adc1b27a2bf668a4
Angie <vett200701@gmail.com>
2025-09-03 09:53:06 -0600

    Add files via upload

---
+++ EVA1_7_AREA DE TRIANGULO/src/eva1_7_area/de/triangulo/EVA1_7_AREADETRIANGULO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_area.de.triangulo;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA1_7_AREADETRIANGULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double area,base,altura;
+        base=15;
+        altura=10;
+        area=(base*altura)/2;
+        System.out.println("El área de un triangulo con base=15 y altura=10 es:");
+        System.out.println(area);
+        
+    }
+    
+}
