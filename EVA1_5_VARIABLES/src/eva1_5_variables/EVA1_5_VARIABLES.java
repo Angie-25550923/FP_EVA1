f5a8cebe45b0c17a8d5ad7f9bdd35f4c575b5182
Angie <vett200701@gmail.com>
2025-09-03 09:51:18 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //DECLARAR  LAS VARIABLES
+        //TIPO DE DATO     NOMBRE DEL IDENTIFICADOR
+        String Nombre;
+        int Edad;
+        double salario;
+        //INICIALIZACIÓN
+        //IDENTIFICADOR:VALOR;
+        //= ----OPERADOR  DE ASIGNACIÓN
+        Nombre= "ANGELES ANTONIO";
+        Edad= 17;
+        salario=1.1;
+        System.out.println(Nombre);
+        System.out.println(Edad);
+        System.out.println(salario);
+        
+    }
+    
+}
