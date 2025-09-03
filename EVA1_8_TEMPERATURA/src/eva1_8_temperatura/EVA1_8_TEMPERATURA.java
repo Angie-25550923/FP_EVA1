3c201352e873c815378d1cca2d0d1fa4d82b07e3
Angie <vett200701@gmail.com>
2025-09-03 09:53:22 -0600

    Add files via upload

---
+++ EVA1_8_TEMPERATURA/src/eva1_8_temperatura/EVA1_8_TEMPERATURA.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_temperatura;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA1_8_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double Fahrenheit, Celsius;
+        Fahrenheit = 89;
+        Celsius=5*(Fahrenheit-32)/9;
+        System.out.println("Fahrenheir=89 en celsius son:");
+        System.out.println(Celsius);
+
+    }
+    
+}
