c58aeffb93cf800f081e31439fc0ed90257a531f
Angie <vett200701@gmail.com>
2025-09-03 09:51:59 -0600

    Add files via upload

---
+++ EVA1_6_OPERACIONES/src/eva1_6_operaciones/EVA1_6_OPERACIONES.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_6_operaciones;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA1_6_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double pi,radio,área;
+        //Es lo mismo que ponerlo por separado
+        //inicialización
+         pi=3.1416;
+        radio=10;
+        área=pi*radio*radio;
+        //salida de los datos
+        System.out.println("El área del circulo del radio=10 es");
+        System.out.println(área);
+    }
+    
+}
