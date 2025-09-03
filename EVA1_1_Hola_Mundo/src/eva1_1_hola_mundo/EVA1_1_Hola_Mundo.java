28132ad7e6bbae44d6b11cd77421fa084101bc42
Angie <vett200701@gmail.com>
2025-09-03 09:41:48 -0600

    Add files via upload

---
+++ EVA1_1_Hola_Mundo/src/eva1_1_hola_mundo/EVA1_1_Hola_Mundo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA1_1_Hola_Mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!!"); 
+        System.out.println("FUNDAMENTOS DE PROGRMACION");
+        
+    }
+    
+}
